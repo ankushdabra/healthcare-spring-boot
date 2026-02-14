39a77eb2bdd7a88f5f6f5da8910963030b599191
Ankush Dabra <ankush.singh.dabra@gmail.com>
2026-02-15 00:51:59 +0530

    Appointment api enhanced

--- src/main/java/com/healthcare/repository/AppointmentRepository.java
+++ src/main/java/com/healthcare/repository/AppointmentRepository.java
@@ -24,6 +24,4 @@ public interface AppointmentRepository extends JpaRepository<AppointmentEntity, 
     @Query("SELECT a FROM AppointmentEntity a WHERE a.doctor.id = :doctorId AND (a.appointmentDate = :currentDate AND a.appointmentTime >= :currentTime) ORDER BY a.appointmentDate ASC, a.appointmentTime ASC")
     List<AppointmentEntity> findTodayAppointmentsForDoctor(@Param("doctorId") UUID doctorId, @Param("currentDate") LocalDate currentDate, @Param("currentTime") LocalTime currentTime);
 
-    List<AppointmentEntity> findByPatientId(UUID patientId);
-    List<AppointmentEntity> findByDoctorId(UUID doctorId);
 }

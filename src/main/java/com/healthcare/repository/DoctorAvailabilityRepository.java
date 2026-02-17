b418518871c6c89923db710df5bf42d7edf9cb5a
Ankush Dabra <ankush.singh.dabra@gmail.com>
2026-02-18 00:45:05 +0530

    Issue fixed

--- src/main/java/com/healthcare/repository/DoctorAvailabilityRepository.java
+++ src/main/java/com/healthcare/repository/DoctorAvailabilityRepository.java
@@ -2,6 +2,7 @@ package com.healthcare.repository;
 
 import com.healthcare.entity.DoctorAvailabilityEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,5 +12,6 @@ public interface DoctorAvailabilityRepository extends JpaRepository<DoctorAvaila
     List<DoctorAvailabilityEntity> findByDoctorId(UUID doctorId);
     List<DoctorAvailabilityEntity> findByDoctorIdIn(List<UUID> doctorIds);
     Optional<DoctorAvailabilityEntity> findTopByDoctorIdOrderByDayAscStartTimeAsc(UUID doctorId);
+    @Modifying
     void deleteByDoctorId(UUID doctorId);
 }
